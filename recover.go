d2244ac4a34b8e9a734cbc0bdaf93c2ce8c36237
Rudolph Jacobs <rudolph@kalido.me>
2025-05-02 12:49:48 +0200

    Add wrapper for capturing panic with custom recovery.

--- recover.go
+++ recover.go
@@ -14,10 +14,14 @@ import (
 //
 // See https://play.golang.org/p/FYD2QlmHHO9
 //
-// Usage
-// 	defer func() {
-// 		err = helpers.ErrRecover(recover())
-// 	}()
+// Usage:
+//
+//	defer func() {
+//		errR := helpers.ErrRecover(recover())
+//		if errR != nil {
+//			err = errR
+//		}
+//	}()
 func ErrRecover(errI interface{}) error {
 	if errI != nil {
 		switch v := errI.(type) {
@@ -34,12 +38,45 @@ func ErrRecover(errI interface{}) error {
 // recovery code. If a panic occurs, it is captured and instead
 // converted to a regular error.
 func WrapEgGoWithRecover(f func() error) func() error {
+	return WrapEgGoWithCustomRecover(f, func(err error) error { return err })
+}
+
+// ErrorHandler represents a function which performs some action on an error
+// which was converted from a panic.
+//
+// This function should return as soon as possible if an error group was
+// canceled.
+type ErrorHandler func(error) error
+
+// WrapEgGoWithCustomRecover takes an input function f and wraps it in panic
+// recovery code. If a panic occurs, it is captured and instead
+// converted to a regular error. This error is then passed to recovery, so that
+// recovery can decide what happens to the error.
+//
+// Example:
+//
+//	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+//	errChan := make(chan error, 1)
+//	wrappedFunc := panicgroup.WrapEgGoWithCustomRecover(
+//		func() {
+//			panic("AAAAAAAH!")
+//		},
+//		func(err error) error {
+//			select {
+//			case <-ctx.Done():
+//				return ctx.Err()
+//			case errChan<-err:
+//				return err
+//			}
+//		}
+//	)
+func WrapEgGoWithCustomRecover(f func() error, recovery ErrorHandler) func() error {
 	return func() (err error) {
 		defer func() {
 			errR := ErrRecover(recover())
 			// Only override the error returned if the error recovered is not nil
 			if errR != nil {
-				err = errR
+				err = recovery(errR)
 			}
 		}()
 
